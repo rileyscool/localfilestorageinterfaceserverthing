08c4ea3e34413fc48e92f2db1efec7e87d1db115
Riley <87399106+rileyscool@users.noreply.github.com>
2024-12-10 20:41:29 +1300

    its very errory but i cbf fixing at this moment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 
 app.use(cookieParser()); // oh my god i should have just read the docs instead of this shit
 app.set("view engine", "ejs")
+app.use(express.json());
 
 const fs = require('fs')
 const config = require("./config.json")
@@ -30,6 +31,18 @@ app.get("/dashboard", (req, res) =>{
     res.render('dashboard')
 })
 
+app.get("/files/*", (req, res) =>{
+    baseUrl = req.url
+    user = baseUrl.split("/")[2]
+    filename = baseUrl.split("/")[3]
+    fileLocation = __dirname + `/users/${user}/${filename}`
+    if(fs.existsSync(fileLocation)){
+        res.sendFile(fileLocation)
+    }else{
+        res.status(404).send("No such file g")
+    }
+})
+
 
 app.post("/accountFiles", (req, res) =>{ // this makes more sense to be a get but too bad
     desiredAccount = req.cookies.username || null
@@ -41,24 +54,26 @@ app.post("/accountFiles", (req, res) =>{ // this makes more sense to be a get bu
     userFiles = fs.readdirSync(`./users/${desiredAccount}`, { withFileTypes: true, recursive: true }) // idk if this includes folders or not so, we will see.
 
     res.send({files: userFiles}) // seems wrong, we will see later
+    // not wrong, good enough!
 
 })
 
 app.post('/account', (req, res) =>{
     // dont judge me because i cant be bothered adding like encryption or some shit for this because its local
     let stuff = req.body.stuff
-    let username = req.body.username
-    let password = req.body.password
-    let action = req.body.action
+    let username = stuff.username
+    let password = stuff.password
+    let action = stuff.action
 
+    if(!fs.existsSync("./users/users.json")){
+        // wow the first user to sign up/login!! we should give them an award
+        let dataToWrite = {}
+        fs.writeFileSync("./users/users.json", JSON.stringify(dataToWrite))
+    }
+    console.log(action)
+    console.log(req.body)
     if(action === "register"){
         // also im gonna be stupid and store userdata in a json file 💀💀
-        if(!fs.existsSync("./users/users.json")){
-            // wow the first user to sign up!! we should give them an award
-            let dataToWrite = {}
-            fs.writeFileSync("./users/users.json", JSON.stringify(dataToWrite))
-        }
-
         let allData = JSON.parse(fs.readFileSync("./users/users.json")) // ew ikr
         if(allData[username]){
             res.status(418).send({"Error": "Username is taken"}) // im a teapot heehe
@@ -68,7 +83,8 @@ app.post('/account', (req, res) =>{
             allData[username] = {pass: password}
             fs.writeFileSync("./users/users.json", JSON.stringify(allData))
             // i hope multiple people dont sign up at the same time
-            res.redirect(`/login`)
+            res.status(200).send("wow")
+            // res.redirect(`/login`)
             fs.mkdirSync(`./users/${username}`) // make a folder for the users files
         }
     }else{
@@ -79,7 +95,7 @@ app.post('/account', (req, res) =>{
             // ^^ ai suggestion 💀 stupid shit
             if(allData[username] === password){
                 res.cookie("username", allData[username]) // cbf doing auth so this is good enough authentication for me hahaha
-                res.redirect("/dashboard")
+                res.status(200)
             }{
                 res.status(418).send({"Error": "Username or password is incorrect"}) // its the username
             }
@@ -91,3 +107,4 @@ app.post('/account', (req, res) =>{
 })
 
 app.listen(config.port || 1234)
+console.log(`Page open at https://localhost:${config.port || 1234}/`)
