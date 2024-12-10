d3b8ecd7b6dec193cb6032a57ca9980de0759d76
Riley <87399106+rileyscool@users.noreply.github.com>
2024-12-10 19:01:05 +1300

    update again while i pause

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ app.get("/", (req, res) =>{
 
 })
 
+res.get("/login", (req, res) =>{
+
+})
+
+res.get("/register", (req, res) =>{
+
+})
+
+res.get("/dashboard", (req, res) =>{
+
+})
+
+
+
 app.post('/account', (req, res) =>{
     // dont judge me because i cant be bothered adding like encryption or some shit for this because its local
     let stuff = req.body.stuff
@@ -24,12 +38,36 @@ app.post('/account', (req, res) =>{
             let dataToWrite = {}
             dataToWrite[username] = {pass: password}
             fs.writeFileSync("./users/users.json", JSON.stringify(dataToWrite))
+            res.redirect(`/login`) // make them login again because i can
+            fs.mkdirSync(`./users/${username}`) // make a folder for the users files
         }
         let allData = JSON.parse(fs.readFileSync("./users/users.json")) // ew ikr
         if(allData[username]){
             res.status(418).send({"Error": "Username is taken"}) // im a teapot heehe
+            // the AI completion thing is so ass, it suggested i send allData ( AI is not taking over the world any time soon )
+        }else{
+            // im not gonna check password validity because im lazy asf and i might do it on clientside if i even add it!
+            allData[username] = {pass: password}
+            fs.writeFileSync("./users/users.json", JSON.stringify(allData))
+            // i hope multiple people dont sign up at the same time
+            res.redirect(`/login`)
+            fs.mkdirSync(`./users/${username}`) // make a folder for the users files
         }
     }else{
+        // they are attempting to login
+        let allData = JSON.parse(fs.readFileSync("./users/users.json")) // ew again
+        if(allData[username]){
+            // res.status(418).send({"Error": "Username is taken"})
+            // ^^ ai suggestion 💀 stupid shit
+            if(allData[username] === password){
+                res.cookie("username", allData[username]) // cbf doing auth so this is good enough authentication for me hahaha
+                res.redirect("/dashboard")
+            }{
+                res.status(418).send({"Error": "Username or password is incorrect"}) // its the username
+            }
+        }else{
+            res.status(418).send({"Error": "Username or password is incorrect"}) // its the username
+        }
 
     }
 })
